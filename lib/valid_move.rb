66fc47c77143a07e24e88f468b1c0e13f5a33cc3
Simon Jacobs <simonadlerjacobs@gmail.com>
2020-10-21 15:06:10 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,13 +1,11 @@
 def valid_move?(board, index)
-  def position_taken?(board, index)
-    return (board[index] == "X" || board[index] == "O")
-  end
-  def on_board?(index)
-    if index.between?(0, 8)
-      return true
-    end
-  end
   if (position_taken?(board, index) == false && on_board?(index) == true)
     return true
   end
+end
+def position_taken?(board, index)
+  board[index] != " "
+end
+def on_board?(num)
+  num.between?(0, 8)
 end
